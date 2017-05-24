4a26124db0af150460cb5bacc3318d672d3c3aea
Rob <rob.mcadoo@gmail.com>
2017-05-25 00:13:50 +0100

    Fixing tasks.py config and code
    Making the celery task read from the correct Redis host.
    
    Also, ensuring that the database code actually inserts into the
    database. TODO: Error handling

--- ingestionapi/tasks.py
+++ ingestionapi/tasks.py
@@ -1,16 +1,22 @@
 from celery import Celery
 from db import Records, db
+
 import json
-import hashlib
+import uuid
+import os
+import time
 
 
-app = Celery('tasks', broker='redis://127.0.0.1:6379/0')
+broker = os.environ.get('TASK_BROKER', 'redis://redis:6379/0')
+app = Celery('tasks', broker=broker)
 
 
 @app.task(ignore_result=True, task_acks_late=True)
 def add(txt):
     r = Records(
             timestamp=int(time.time()),
-            message_id=hashlib.sha1(txt).hexdigest()[0:7],
+            message_id=uuid.uuid4().hex,
             record=txt
     )
+    db.session.add(r)
+    db.session.commit()

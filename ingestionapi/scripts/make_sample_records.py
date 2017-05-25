c36f9e8d6d6b74668e7521530491b8dba6e778f7
Rob <rob.mcadoo@gmail.com>
2017-05-25 06:35:50 +0100

    py3 compatibility issue s/xrange/range

--- ingestionapi/scripts/make_sample_records.py
+++ ingestionapi/scripts/make_sample_records.py
@@ -7,7 +7,7 @@ make_uuid = lambda: str(uuid.uuid4().hex)
 
 # Create a bunch of boring records.
 # If you get tired of waiting just hit ^C
-for i in xrange(100000):
+for i in range(100000):
     r = Records(timestamp=make_time(), message_id=make_uuid(), record='a'*1000)
     db.session.add(r)
     # Commit records periodically.
